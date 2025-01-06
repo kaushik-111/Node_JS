9a267510294cde539796a7e10eaa67fa12028b01
kaushik-111 <166016754+kaushik-111@users.noreply.github.com>
2025-01-06 16:07:52 +0530

    commited

---
+++ Day-1/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const port = 1018;
+
+const portHandler = (req,res) =>{
+    res.write("<h1>Server Started</h1>");
+    res.end();
+}
+const server = http.createServer(portHandler);
+
+server.listen(port,(err)=>{
+    err ? console.log(err) : console.log("server side on port:" +port);
+})
